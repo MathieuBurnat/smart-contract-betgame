fa532a5ff1cfe6646fe3ac475c997616b1032937
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-10-04 11:19:45 +0200

    add casino contract

--- betme.sol
+++ betme.sol
@@ -4,9 +4,8 @@ contract Ownable {
     address owner;
 
     function Ownable() public {
-        // Set owner to who creates the contract
         owner = msg.sender;
-    } // Access modifier
+    }
 
     modifier Owned() {
         require(msg.sender == owner);
@@ -15,8 +14,44 @@ contract Ownable {
 }
 
 contract Mortal is Ownable {
-    // Our access modifier is present, only the contract creator can      use this function
     function kill() public Owned {
         selfdestruct(owner);
+    }
+}
+
+contract Casino is Mortal {
+    uint256 minBet;
+    uint256 houseEdge; //in %
+
+    event Won(bool _status, uint256 _amount);
+
+    function Casino(uint256 _minBet, uint256 _houseEdge) public payable {
+        require(_minBet > 0);
+        require(_houseEdge <= 100);
+        minBet = _minBet;
+        houseEdge = _houseEdge;
+    }
+
+    function() public {
+        //fallback
+        revert();
+    }
+
+    function bet(uint256 _number) public payable {
+        require(_number > 0 && _number <= 10);
+        require(msg.value >= minBet);
+        uint256 winningNumber = (block.number % 10) + 1;
+        if (_number == winningNumber) {
+            uint256 amountWon = (msg.value * (100 - houseEdge)) / 10;
+            if (!msg.sender.send(amountWon)) revert();
+            emit Won(true, amountWon);
+        } else {
+            emit Won(false, 0);
+        }
+    }
+
+    function checkContractBalance() public view Owned returns (uint256) {
+        address _contract = this;
+        return _contract.balance;
     }
 }
